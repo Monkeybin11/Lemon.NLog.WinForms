f42b28b5e677ec360e4e3cee7c4a35e6cbee7f4c
Hannele Ruiz <justlemoncl@gmail.com>
2020-02-29 17:56:43 -0300

    Added comment for the existing code

--- Lemon.NLog.WinForms/TextBoxTarget.cs
+++ Lemon.NLog.WinForms/TextBoxTarget.cs
@@ -5,9 +5,15 @@ using System.Windows.Forms;
 
 namespace Lemon.NLog.WinForms
 {
+    /// <summary>
+    /// Logs text into a Text Box control in an existing form.
+    /// </summary>
     [Target("TextBox")]
     public class TextBoxTarget : TargetWithContext
     {
+        /// <summary>
+        /// The TextBox control that will be used for this
+        /// </summary>
         public TextBox TargetTextBox { get; private set; }
 
         public TextBoxTarget(TextBox textBox) : base()
@@ -17,8 +23,11 @@ namespace Lemon.NLog.WinForms
 
         protected override void Write(LogEventInfo LogEvent)
         {
+            // If a handle has not been created for this TextBox
             if (!TargetTextBox.IsHandleCreated)
             {
+                // Get the pointer/handle of the TextBox (is silently created if not)
+                // And only call Invoke when we are not in the UI Thread (speed!)
                 if (TargetTextBox.InvokeRequired)
                 {
                     TargetTextBox.Invoke(new Action(() => { IntPtr pointer = TargetTextBox.Handle; }));
@@ -29,6 +38,7 @@ namespace Lemon.NLog.WinForms
                 }
             }
 
+            // Then, just go ahead and append the message
             TargetTextBox.Invoke(new Action(() => TargetTextBox.AppendText(Layout.Render(LogEvent) + Environment.NewLine)));
         }
     }
