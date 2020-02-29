b2c3b3829cce4b16415ccc7e4f411195300dbccd
Hannele Ruiz <justlemoncl@gmail.com>
2020-02-29 17:15:50 -0300

    Added existing target for TextBox Controls (from PatchMyPath)

---
+++ Lemon.NLog.WinForms/TextBoxTarget.cs
@@ -0,0 +1,35 @@
+﻿using NLog;
+using NLog.Targets;
+using System;
+using System.Windows.Forms;
+
+namespace Lemon.NLog.WinForms
+{
+    [Target("TextBox")]
+    public class TextBoxTarget : TargetWithContext
+    {
+        public TextBox TextBox { get; }
+
+        public TextBoxTarget(TextBox textBox) : base()
+        {
+            TextBox = textBox;
+        }
+
+        protected override void Write(LogEventInfo LogEvent)
+        {
+            if (!TextBox.IsHandleCreated)
+            {
+                if (TextBox.InvokeRequired)
+                {
+                    TextBox.Invoke(new Action(() => { IntPtr pointer = TextBox.Handle; }));
+                }
+                else
+                {
+                    IntPtr pointer = TextBox.Handle;
+                }
+            }
+
+            TextBox.Invoke(new Action(() => TextBox.AppendText(Layout.Render(LogEvent) + Environment.NewLine)));
+        }
+    }
+}
